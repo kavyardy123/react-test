4f9fb2f63327c8e8b6be0bf16879ca70bde58c2e
kavyardy123 <kavyardy5240@gmail.com>
2021-06-04 08:00:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-hzsrwp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-hzsrwp)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+export default function (){
+  return(
+    <div>
+    <h1> Hello Kavya reddy </h1>
+    <p>welcome to javascript..</p>
+    </div>
+    
+    
+  )
+}
